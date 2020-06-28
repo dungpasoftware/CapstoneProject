70b4a1e0a1bff78c87ce99ec1cc041a251285446
RegularGuy1998 <bujduk@gmail.com>
2020-06-29 00:50:01 +0700

    Doing cashier screen

--- restaurant-web/src/containers/BackendNavigation.js
+++ restaurant-web/src/containers/BackendNavigation.js
@@ -9,14 +9,26 @@ function BackendNavigation() {
 
     return (
             <ul className="body-menu">
-                <li className={`menu-item ${(fullPath.includes(`${url}/food`)) ? 'active' : ''}`}>
-                    <Link className="item__link" to={`${url}/food`}>
+                <li className={`menu-item ${(fullPath.includes(`${url}/cashier`)) ? 'active' : ''}`}>
+                    <Link className="item__link" to={`${url}/cashier`}>
                         <div className="item__icon">
-                            <i className="fas fa-drumstick-bite"></i>
+                            <i className="fad fa-cash-register"/>
                         </div>
                         <div className="item__hover">
                             <div className="item__hover--header">
-                                Thực đơn
+                                Thu ngân
+                            </div>
+                        </div>
+                    </Link>
+                </li>
+                <li className={`menu-item ${(fullPath.includes(`${url}/food`)) ? 'active' : ''}`}>
+                    <Link className="item__link" to={`${url}/food`}>
+                        <div className="item__icon">
+                            <i className="fas fa-drumstick-bite"/>
+                        </div>
+                        <div className="item__hover">
+                            <div className="item__hover--header">
+                                Quản lý thực đơn
                             </div>
                         </div>
                     </Link>
