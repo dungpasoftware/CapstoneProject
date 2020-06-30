08250f7d19b08014473ff82b464d9d9067f25e3d
nhandayht <kynhanht@gmail.com>
2020-06-30 16:12:27 +0700

    update locationtable and table version 2

--- restaurant/src/main/java/fu/rms/controller/LocationTableController.java
+++ restaurant/src/main/java/fu/rms/controller/LocationTableController.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import fu.rms.dto.LocationTableDto;
@@ -21,9 +21,8 @@ public class LocationTableController {
 		
 		return locationTableService.findAll();
 	}
-	
-	@GetMapping("/locationTable")
-	public LocationTableDto getLocationTable(@RequestParam Long locationId){
+	@GetMapping("/locationTable/{id}")
+	public LocationTableDto getLocationTable(@PathVariable(name = "id") Long locationId){
 		
 		return locationTableService.findByLocationId(locationId);
 	}
--- restaurant/src/main/java/fu/rms/controller/TableController.java
+++ restaurant/src/main/java/fu/rms/controller/TableController.java
@@ -2,6 +2,7 @@ package fu.rms.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,17 +15,15 @@ public class TableController {
 
 	@Autowired
 	private ITableService tableService;
-	
-	@GetMapping("/table")
-	public TableDto getTable(@RequestParam Long tableId) {
-		
-			return tableService.findByTableId(tableId);
+
+	@GetMapping("/table/{id}")
+	public TableDto getTable(@PathVariable("id") Long tableId) {
+		return tableService.findByTableId(tableId);
 	}
 
 	@PutMapping("/table")
 	public TableDto updateStatusTable(@RequestParam Long tableId, Long status) {
 		return tableService.updateStatus(tableId, status);
 	}
-	
-	
+
 }
