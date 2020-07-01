e2a12ccab1d4ae3252f934d979b3fa7bb2c98a46
Minh Duc <duc.bui@itnavi.com>
2020-07-02 05:42:05 +0700

    remove old web

--- restaurant-web/src/networks/Auth.js
+++
@@ -1,6 +0,0 @@
-import Axios from "axios";
-import {ROOT_API} from "../static";
-
-export const loginUser = ({username, password}) => {
-    return Axios.post(`${ROOT_API}/auth`, {username, password});
-}
