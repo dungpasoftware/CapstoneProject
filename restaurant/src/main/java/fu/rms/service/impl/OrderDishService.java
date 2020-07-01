c506a3107f1da9791cc5356e4df72b6396b0bf98
unknown <nganduc98@gmail.com>
2020-07-02 02:18:40 +0700

    orderdish

--- restaurant/src/main/java/fu/rms/service/impl/OrderDishService.java
+++ restaurant/src/main/java/fu/rms/service/impl/OrderDishService.java
@@ -1,10 +1,31 @@
 package fu.rms.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fu.rms.dto.OrderDishDto;
+import fu.rms.entity.OrderDish;
+import fu.rms.mapper.OrderDishMapper;
+import fu.rms.repository.OrderDishRepository;
 import fu.rms.service.IOrderDishService;
 
 @Service
 public class OrderDishService implements IOrderDishService {
+	
+	@Autowired
+	OrderDishMapper orderDishMapper;
+	
+	@Autowired
+	OrderDishRepository orderDishRepo;
+
+	@Override
+	public List<OrderDishDto> getListOrderDishByOrder(Long orderId) {
+
+//		List<OrderDish> listOrderDish = orderDishRepo.findByOrderId(Long orderId);
+		
+		return null;
+	}
 
 }
