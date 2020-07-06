7bb7e0e65b1018c69c5b2c2ffdac29d95de5f8c4
nhandayht <kynhanht@gmail.com>
2020-07-06 23:39:32 +0700

    update category

--- restaurant/src/main/java/fu/rms/controller/CategoryController.java
+++ restaurant/src/main/java/fu/rms/controller/CategoryController.java
@@ -1,14 +1,10 @@
 package fu.rms.controller;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fu.rms.dto.CategoryDto;
@@ -26,8 +22,8 @@ public class CategoryController {
 	}
 
 	@GetMapping("/categories/{id}")
-	public CategoryDto categoryById(@PathVariable Long id, @RequestParam Boolean isGetDishes ) {
-		return iCategoryService.getById(id,isGetDishes);
+	public CategoryDto categoryById(@PathVariable Long id) {
+		return iCategoryService.getById(id);
 	}
 
 }
