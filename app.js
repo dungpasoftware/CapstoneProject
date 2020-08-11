3671dfc9abdd0a3602b5dc7ddab7073210a12a51
Minh Duc <duc.bui@itnavi.com>
2020-08-11 13:10:40 +0700

    fix Procfile

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.urlencoded());
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello World! cai ma cha may');
 });
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
